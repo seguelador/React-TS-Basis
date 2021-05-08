f84a2482db59db572319e6e8e881a5d94baeedba
Matias Seguel <mseguelestay@gmail.com>
2021-05-08 13:46:10 -0400

    Basic uses of TS in React

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import ReactDOM from 'react-dom';
 /* import GuestList from './state/GuestList'; */
-import UserSearch from './state/UserSearch';
+/* import EventComponent from './events/EventComponent'; */
+import UserSearch from './refs/UserSearch';
 
 const App = () => {
     return <div>
--- src/refs/UserSearch.tsx
+++ src/refs/UserSearch.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useRef, useEffect } from 'react';
 
 const users = [
     { name: 'Sandra', age: 20 },
@@ -7,8 +7,17 @@ const users = [
 ];
 
 const UserSearch: React.FC = () => {
+    const inputRef = useRef<HTMLInputElement | null>(null);
     const [name, setName] = useState('');
     const [user, setUser] = useState<{ name: string, age: number} | undefined>();
+
+    useEffect(() => {
+        /* inputRef.current?.focus() */
+        if (!inputRef.current) {
+            return;
+        }
+        inputRef.current.focus()
+    }, []);
 
     const onClick = () => {
         const foundUser = users.find((user) => {
@@ -21,7 +30,7 @@ const UserSearch: React.FC = () => {
     return (
         <div>
             UserSearch
-            <input value={name} onChange={e => setName(e.target.value)} />
+            <input ref={inputRef} value={name} onChange={e => setName(e.target.value)} />
             <button onClick={onClick}> Find User </button>
             <div>
                 {user && user.name}
